027b0784b4123db5b9e97f2a51c99c6959cb39e5
sdremora <sdremora@sinys.ru>
2019-02-24 12:40:42 +0300

    ttt

--- src/general.c
+++ src/general.c
@@ -1,8 +1,10 @@
 
 #include "lem_in.h"
 
+#include <stdio.h>
+
 void	error_handle(int error_index, char *error_str)
 {
-	ft_printf("%s\n", error_str);
+	printf("%s\n", error_str);
 	exit(error_index);
 }
