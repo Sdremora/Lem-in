8ea3170d306faee83b81c4ee9d4a214930aa33df
sdremora <sdremora@sinys.ru>
2019-02-26 12:12:21 +0300

    before eval

--- include/lem_in/way_finder.h
+++ include/lem_in/way_finder.h
@@ -4,14 +4,22 @@
 
 # include "general.h"
 
+# define E_NOPATH -5
+# define E_NOPATH_STR "Error\n"
+
 typedef	struct	s_resolve
 {
-	int			nb;
-	t_arrnum	*path;
-	t_list		*path_list;
+	t_path		**path_ar;
 	int			flow_count;
 	int			move_count;
 }				t_resolve;
+
+typedef struct	s_path
+{
+	t_room		**ar;
+	int			size;
+	int			max_size;
+}				t_path;
 
 t_list *way_finder(t_farm *farm);
 
--- src/way_finder.c
+++ src/way_finder.c
@@ -1,145 +1,291 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   way_finder.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: sdremora <sdremora@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/23 16:22:16 by hharvey           #+#    #+#             */
-/*   Updated: 2019/02/24 18:12:51 by sdremora         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "lem_in.h"
 #include "libft.h"
 
-void		add_pre_room(t_list **lst, t_room *pre_room)
-{
-	t_list	*node;
+/*
+**	2 -> content exist in lst and have different step
+**	1 -> content exist in lst and have same step
+**	0 -> not exist
+*/
 
-	node = ft_lstput(pre_room, sizeof(*pre_room));
-	if (!node)
+int		check_exist(t_list *lst, t_room *found_room, int step)
+{
+	while (lst)
+	{
+		if (lst->content == found_room)
+		{
+			if (((t_room *)lst->content)->step == step)
+				return (1);
+			return (2);
+		}
+		lst = lst->next;
+	}
+	return (0);
+}
+
+#define R_END 1
+
+int		add_mark(t_room *next_room, t_room *room, int step)
+{
+ 	t_list	*node;
+	int		status;
+
+	status = check_exist(room->pre_list, next_room, step);
+	if (status == 2)
+		return (0);
+ 	node = ft_lstput(room, sizeof(t_room));
+ 	if (!node)
+ 		error_handle(E_NOMEM, E_NOMEM_STR);
+ 	ft_lstadd(&next_room->pre_list, node);
+	if (next_room->pre_list->next != NULL ||\
+		status != 0 ||\
+		next_room->type == R_END)
+		return (0);
+	return (1);
+}
+
+ t_list		*add_link(t_list **next_room_lst, t_room *room, int step) // room = 3
+ {
+ 	t_list	*link_lst;
+ 	t_list	*new_node;
+ 	t_room	*next_room;
+
+ 	link_lst = room->link_list;
+ 	while (link_lst != NULL)
+ 	{
+ 		next_room = (t_room *)link_lst->content; // next_room = 1
+ 		if (add_mark(next_room, room, step))
+		{
+			next_room->step = step;
+			new_node = ft_lstput(next_room, sizeof(t_room));
+			if (new_node == NULL)
+				error_handle(E_NOMEM, E_NOMEM_STR);
+			ft_lstadd(next_room_lst, new_node);
+		}
+ 		link_lst = link_lst->next;
+ 	}
+ }
+
+ int		marker(t_list *room_lst, int step)
+ {
+ 	t_list	*next_room_lst;
+
+ 	if (!room_lst)
+ 		return (step);
+	step++;
+ 	while (room_lst)
+ 	{
+ 		add_link(&next_room_lst, (t_room *)room_lst->content, step);
+ 		room_lst = room_lst->next;
+ 	}
+ 	return (marker(next_room_lst, step));
+ }
+
+t_list		*path_combine(t_farm *farm)
+{
+	t_list	*path_lst;
+
+	return (path_lst);
+}
+
+t_path	*path_ini(int path_size)
+{
+	t_path	*path;
+
+	if (!(path = (t_path *)malloc(sizeof(t_path))) ||\
+		!(path->ar = (t_room **)ft_memalloc(sizeof(t_room *) * path_size)))
 		error_handle(E_NOMEM, E_NOMEM_STR);
-	ft_lstadd(room->pre_list, node);
+	path->size = 0;
+	path->max_size = path_size;
+	return (path);
 }
 
-t_list		*add_link(t_list **rooms, t_room *room)
+void	path_to_lst(t_list **lst, t_path *path)
+{
+	t_list	*path_node;
+
+	path_node = ft_lstput(path, sizeof(t_path));
+	if (!path_node)
+		error_handle(E_NOMEM, E_NOMEM_STR);
+	ft_lstadd(lst, path_node);
+}
+
+void	path_clear(t_path *path)
+{
+	free(path->ar);
+	free(path);
+}
+
+t_path	*path_get_copy(t_path *path)
+{
+	t_path	*path_copy;
+	int		i;
+
+	path_copy = path_ini(path->max_size);
+	i = 0;
+	while (i < path->size)
+	{
+		path_copy->ar[i] = path->ar[i];
+		i++;
+	}
+	path_copy->size = path->size;
+	return (path_copy);
+}
+
+void	path_builder(t_list **path_lst, t_path *path, t_room *room)
+{
+	if (path->size < 3 || path->ar[path->size - 2] != room)
+	{
+		path->ar[path->size] = room;
+		path->size++;
+		path_logic(path_lst, path, (t_room *)room->pre_list->content);
+	}
+	else
+		path_clear(path);
+}
+
+void	path_logic(t_list **path_lst, t_path *path, t_room *room)
 {
 	t_list	*node;
-	t_list	*new_node;
-	t_room	*next_room;
+	t_list	*path_node;
+	t_path	*path_copy;
 
-	node = room->link_list;
-	while (node != NULL)
+	node = room->pre_list;
+	if (node == NULL)
 	{
-		next_room = (t_room *)node->content;
-		if (next_room->pre_list == NULL)
-		{
-			add_pre_room(next_room, room);
-
-		}
-		new_node = ft_lstput(room, sizeof(t_room));
-		if (new_node == NULL)
-			error_handle(E_NOMEM, E_NOMEM_STR);
-		ft_lstadd(rooms, new_node);
-		node = node->next;
-	}
-}
-
-void		check_confront(t_list *rooms_lst, t_list *next_rooms_lst)
-{
-	t_list	*node;
-	t_list	*next_node;
-	t_room	*room;
-	t_room	*next_room;
-	t_room	*next_pre_room;
-
-	node = rooms_lst;
-	while (node)
-	{
-		room = (t_room *)node->content;
-		next_node = next_rooms_lst;
-		while (next_node)
-		{
-			next_room = (t_room *)next_node->content;
-			if (room == next_room)
-			{
-
-			}
-			next_node = next_node->next;
-		}
-		node = node->next;
-	}
-}
-
-void		marker(t_list *rooms_lst)
-{
-	t_list	*next_rooms_lst;
-	t_list	*room;
-
-	if (!rooms_lst)
+		path_to_lst(path_lst, path);
 		return ;
-	next_rooms_lst = NULL;
-	room = rooms_lst;
-	while (room)
-	{
-		add_link(&next_rooms_lst, room);
-		found_confront(rooms_lst, next_rooms_lst);
-		room = room->next;
 	}
-
-	marker(next_rooms_lst);
+	if (node->next == NULL)
+		path_builder(path_lst, path, (t_room *)node->content);
+	else
+	{
+		while (node->next)
+		{
+			path_copy = path_get_copy(path);
+			path_builder(path_lst, path_copy, (t_room *)node->content);
+			node = node->next;
+		}
+		path_builder(path_lst, path, (t_room *)node->content);
+	}
 }
 
-t_list *way_finder(t_farm *farm)
+t_list	*path_combine(t_farm *farm, int	max_step)
 {
-	t_list	*cur_rooms_list;
-	t_list	*resolve;
+	t_list	*path_lst;
+	t_path	*path;
 
-	cur_rooms_list = ft_lstput(farm->room[0], sizeof(t_room));
-	if (!cur_rooms_list)
+	path_lst = NULL;
+	path = path_ini(max_step);
+	path->ar[0] = farm->end;
+	path->size++;
+	path_logic(&path_lst, path, farm->end);
+	return (path_lst);
+}
+
+t_resolve	*resolve_ini(int flow_count)
+{
+	t_resolve	*resolve;
+
+	if ((resolve = (t_resolve *)malloc(sizeof(t_resolve))) ||\
+		(resolve->path_ar = (t_path **)ft_memalloc(sizeof(t_path *) * flow_count)))
 		error_handle(E_NOMEM, E_NOMEM_STR);
-	marker(cur_rooms_list);
-
+	resolve->flow_count = flow_count;
+	resolve->move_count = -1;
 	return (resolve);
 }
 
-// t_list *way_finder(t_farm *farm)
-// {
-// 	t_list		*resolve;
-// 	t_resolve	*temp;
-// 	t_list		*path1;
-// 	t_list		*path2;
-// 	t_list		*path3;
+t_resolve	*get_resolve(t_list *path_lst, int flow)
+{
+	t_resolve	*resolve;
 
-// 	path1 = NULL;
-// 	path2 = NULL;
-// 	path3 = NULL;
-// 	ft_lstadd(&path1, ft_lstput(farm->room[1], sizeof(t_room)));
-// 	ft_lstadd(&path1, ft_lstput(farm->room[2], sizeof(t_room)));
-// 	ft_lstadd(&path1, ft_lstput(farm->room[4], sizeof(t_room)));
+	resolve = resolve_ini(flow);
+	return (resolve);
+}
 
-// 	ft_lstadd(&path2, ft_lstput(farm->room[1], sizeof(t_room)));
-// 	ft_lstadd(&path2, ft_lstput(farm->room[3], sizeof(t_room)));
-// 	ft_lstadd(&path2, ft_lstput(farm->room[4], sizeof(t_room)));
+void	make_eval(t_farm *farm, t_list **resolve_lst, t_list *path_lst)
+{
+	t_resolve	*resolve;
+	t_list		*node;
+	int			max_flow;
+	int			i;
 
-// 	ft_lstadd(&path3, ft_lstput(farm->room[2], sizeof(t_room)));
-// 	ft_lstadd(&path3, ft_lstput(farm->room[5], sizeof(t_room)));
-// 	ft_lstadd(&path3, ft_lstput(farm->room[4], sizeof(t_room)));
+	i = ft_lstlen(farm->start->link_list);
+	max_flow = ft_lstlen(farm->end->link_list);
+	max_flow = i < max_flow ? i : max_flow;
+	i = 1;
+	while (i <= max_flow)
+	{
+		resolve = get_resolve(path_lst, i);
+		if (resolve == NULL)
+			break;
+		else
+		{
+			node = ft_lstput(resolve, sizeof(t_resolve));
+			if (node == NULL)
+				error_handle(E_NOMEM, E_NOMEM_STR);
+			ft_lstadd(resolve_lst, node);
+		}
+		i++;
+	}
+}
 
-// 	temp = (t_resolve *)ft_memalloc(sizeof(t_resolve));
-// 	temp->path_list = NULL;
-// 	ft_lstadd(&temp->path_list, path1);
-// 	temp->flow_count = 1;
-// 	temp->move_count = 3;
-// 	ft_lstadd(&resolve, ft_lstput(temp, sizeof(t_resolve)));
+ t_list *way_finder(t_farm *farm)
+ {
+ 	t_list	*cur_rooms_list;
+	t_list	*path_lst;
+	t_list	*resolve_lst;
+	int		max_step;
 
-// 	temp = (t_resolve *)ft_memalloc(sizeof(t_resolve));
-// 	temp->path_list = NULL;
-// 	ft_lstadd(&temp->path_list, path2);
-// 	ft_lstadd(&temp->path_list, path3);
-// 	temp->flow_count = 2;
-// 	temp->move_count = 6;
-// 	ft_lstadd(&resolve, ft_lstput(temp, sizeof(t_resolve)));
-// 	return (resolve);
-// }
+	farm->room[0]->step = 0;
+ 	cur_rooms_list = ft_lstput(farm->room[0], sizeof(t_room));
+ 	if (!cur_rooms_list)
+ 		error_handle(E_NOMEM, E_NOMEM_STR);
+ 	max_step = marker(cur_rooms_list, 0);
+	if (farm->end->pre_list == NULL)
+		error_handle(E_NOPATH, E_NOPATH_STR);
+	path_lst = path_combine(farm, max_step);
+	resolve_lst = NULL;
+	make_eval(farm, &resolve_lst, path_lst);
+	return (resolve_lst);
+ }
+
+t_list *way_finder(t_farm *farm)
+{
+	t_list		*resolve;
+	t_resolve	*temp;
+	t_list		*path1;
+	t_list		*path2;
+	t_list		*path3;
+
+	path1 = NULL;
+	path2 = NULL;
+	path3 = NULL;
+	ft_lstadd(&path1, ft_lstput(farm->room[1], sizeof(t_room)));
+	ft_lstadd(&path1, ft_lstput(farm->room[2], sizeof(t_room)));
+	ft_lstadd(&path1, ft_lstput(farm->room[4], sizeof(t_room)));
+
+	ft_lstadd(&path2, ft_lstput(farm->room[1], sizeof(t_room)));
+	ft_lstadd(&path2, ft_lstput(farm->room[3], sizeof(t_room)));
+	ft_lstadd(&path2, ft_lstput(farm->room[4], sizeof(t_room)));
+
+	ft_lstadd(&path3, ft_lstput(farm->room[2], sizeof(t_room)));
+	ft_lstadd(&path3, ft_lstput(farm->room[5], sizeof(t_room)));
+	ft_lstadd(&path3, ft_lstput(farm->room[4], sizeof(t_room)));
+
+	temp = (t_resolve *)ft_memalloc(sizeof(t_resolve));
+	temp->path_list = NULL;
+	ft_lstadd(&temp->path_list, path1);
+	temp->flow_count = 1;
+	temp->move_count = 3;
+	ft_lstadd(&resolve, ft_lstput(temp, sizeof(t_resolve)));
+
+	temp = (t_resolve *)ft_memalloc(sizeof(t_resolve));
+	temp->path_list = NULL;
+	ft_lstadd(&temp->path_list, path2);
+	ft_lstadd(&temp->path_list, path3);
+	temp->flow_count = 2;
+	temp->move_count = 6;
+	ft_lstadd(&resolve, ft_lstput(temp, sizeof(t_resolve)));
+	return (resolve);
+}
